17282cd175b0e33aabd62971377e2aa23cb361ea
root <root@DESKTOP-8J3G7OT.>
2025-04-14 06:23:56 +0000

    Modified factorial.py to use command-line argument

--- factorial.py
+++ factorial.py
@@ -1,9 +1,16 @@
+import sys
+
 def factorial(n):
-    if n == 0 or n == 1:
+    if n == 0:
         return 1
     else:
-        return n * factorial(n - 1)
+        return n * factorial(n-1)
 
-num = int(input("Enter a number: "))
+# Read the number from the command-line argument
+if len(sys.argv) > 1:
+    num = int(sys.argv[1])
+else:
+    num = 5  # Default to 5 if no argument is provided
+
 print(f"The factorial of {num} is {factorial(num)}")
 
